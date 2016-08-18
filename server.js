e24765caad3002dc080374269f2d07b11f057cc9
Ryan Magoon <mr.magoon5@gmail.com>
2016-08-18 15:53:05 -0400

    changed port to work with heroku

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/:date', function (req, res) {
     }
 });
 
-app.listen(3000, function () {
-    console.log('app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('app started!');
 });
